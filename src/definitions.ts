d073096a32d7b8532eac5a0b21dd774491e7b59e
mjLee <audwn0326@gmail.com>
2021-11-22 17:33:56 +0900

    initial state

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,13 @@
 export interface CapacitorKakaoPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  initializeKakao(options: { appKey: string }): Promise<{ value: string }>;
+  kakaoLogin(): Promise<{ value: string }>;
+  kakaoLogout(): Promise<{ value: string }>;
+  kakaoUnlink(): Promise<{ value: string }>;
+  sendLinkFeed(options: {
+    title: string;
+    description: string;
+    imageUrl: string;
+    imageLinkUrl: string;
+    buttonTitle: string;
+  }): Promise<{ value: string }>;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,110 @@
 import { WebPlugin } from '@capacitor/core';
-
+import * as Kakao from './assets/kakao-sdk';
 import type { CapacitorKakaoPlugin } from './definitions';
 
-export class CapacitorKakaoWeb
-  extends WebPlugin
-  implements CapacitorKakaoPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+export class CapacitorKakaoWeb extends WebPlugin implements CapacitorKakaoPlugin {
+  webKey: any;
+
+  initializeKakao(options: { appKey: string, webKey: string }) {
+    return new Promise<{ value: string }>(resolve => {
+      if (this.webKey === undefined) {
+        this.webKey = options.webKey;
+      }
+      Kakao.init(this.webKey);
+      resolve({ value: 'done' });
+    });
+  }
+
+  //웹 카카오 로그인
+  kakaoLogin() {
+    return new Promise<{ value: string }>((resolve, reject) => {
+      if (!this.webKey) {
+        reject('kakao_sdk_not_initialzed');
+      }
+      const KakaoSdk: any = Kakao;
+      KakaoSdk.Auth.login({
+        success: function (authObj: any) {
+          let { access_token } = authObj;
+          resolve({ value: access_token });
+        },
+        fail: function (err: any) {
+          console.error(err);
+          reject(err);
+        },
+      });
+    });
+  }
+
+  //웹 로그아웃
+  kakaoLogout() {
+    return new Promise<{ value: string }>((resolve, reject) => {
+      if (!this.webKey) {
+        reject('kakao_sdk_not_initialzed');
+      }
+
+      const KakaoSdk: any = Kakao;
+      KakaoSdk.Auth.logout();
+      resolve({ value: 'done' });
+    });
+  }
+
+  //unlink
+  kakaoUnlink() {
+    return new Promise<{ value: string }>((resolve, reject) => {
+      if (!this.webKey) {
+        reject('kakao_sdk_not_initialzed');
+      }
+
+      const KakaoSdk: any = Kakao;
+      KakaoSdk.API.request({
+        url: '/v1/user/unlink',
+        success: function (response: any) {
+          console.log(response);
+          resolve({ value: 'done' });
+        },
+        fail: function (error: any) {
+          console.log(error);
+          reject(error);
+        },
+      });
+    });
+  }
+
+  //message
+  sendLinkFeed(options: {
+    title: string;
+    description: string;
+    imageUrl: string;
+    imageLinkUrl: string;
+    buttonTitle: string;
+  }) {
+    return new Promise<{ value: string }>((resolve, reject) => {
+      if (!this.webKey) {
+        reject('kakao_sdk_not_initialzed');
+      }
+      const KakaoSdk: any = Kakao;
+      KakaoSdk.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: options.title,
+          description: options.description,
+          imageUrl: options.imageUrl,
+          link: {
+            mobileWebUrl: options.imageLinkUrl,
+          },
+        },
+        buttons: [
+          {
+            title: options.buttonTitle,
+            link: {
+              mobileWebUrl: options.imageLinkUrl,
+            },
+          },
+        ],
+        callback: function () {
+          resolve({ value: 'done' });
+        },
+      });
+    });
   }
 }
